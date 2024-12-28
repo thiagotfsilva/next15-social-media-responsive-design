295ac4cb7c9cd62178e161aab893bfdc9e3ff91a
thiagotfsilva <thiagophsilva@hotmail.com>
2024-12-28 14:35:09 -0300

    congig break points

---
+++ src/components/LeftBar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LeftBar = () => {
+  return (
+    <div>
+      LeftBar
+    </div>
+  )
+}
+
+export default LeftBar
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,15 @@ export default {
   ],
   theme: {
     extend: {
+      screens: {
+        xsm: "500px",
+        sm: "600px",
+        md: "690px",
+        lg: "988px",
+        xl: "1078px",
+        xxl: "1265px"
+
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
