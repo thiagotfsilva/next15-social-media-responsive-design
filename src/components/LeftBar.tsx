6cd2e1353b73acc8e71e43f44c0c486b0b315f5f
thiagotfsilva <thiagophsilva@hotmail.com>
2025-01-14 17:16:36 -0300

    implementando leftbar

--- src/components/LeftBar.tsx
+++ src/components/LeftBar.tsx
@@ -69,27 +69,61 @@ const LeftBar = () => {
   return (
     <div className='h-screen sticky top-0 flex flex-col justify-between pt-2 pb-8'>
       {/* Logo MENU BUTTON */}
-      <div className="">
+      <div className="flex flex-col gap-4 text-lg items-center xxl:items-start">
         {/* LOGO */}
-        <Link href="/">
+        <Link href="/" className='p-2 rounded-full hover:bg-[#181818]'>
           <Image src="icons/logo.svg" alt='logo' width={24} height={24} />
         </Link>
         {/* MENU LIST */}
-      <div className="flex flex-col">
-        {menuList.map((item) => (
-          <Link href={item.link} className='' key={item.id}>
-            <Image
-             src={`icons/${item.icon}`}
-             alt={item.name}
-             width={24}
-             height={24}
-            />
-          </Link>
-        ))}
-      </div>
+        <div className="flex flex-col gap-4">
+          {menuList.map((item) => (
+            <Link
+            href={item.link}
+            className='p-2 rounded-full hover:bg-[#181818] flex items-center gap-4'
+            key={item.id}
+            >
+              <Image
+              src={`icons/${item.icon}`}
+              alt={item.name}
+              width={24}
+              height={24}
+              />
+              <span className='hidden xxl:inline'>{item.name}</span>
+            </Link>
+          ))}
+        </div>
+        {/* BUTTON */}
+        <Link
+          href="/"
+          className='xxl:hidden bg-white text-black rounded-full font-bold w-12 h-12 flex items-center justify-center'
+        >
+          <Image
+           src="icons/post.svg"
+           alt='new post'
+           width={24}
+           height={24}
+          />
+        </Link>
+        <Link
+          href="/"
+          className='hidden xxl:block bg-white text-black rounded-full font-bold py-2 px-20'
+        >
+          Post
+        </Link>
       </div>
       {/* USER */}
-      <div className="">user</div>
+      <div className="flex items-center justify-between">
+        <div className='flex items-center gap-2'>
+          <div className="w-10 h-10 relative rounded-full overflow-hidden">
+            <Image src="/general/avatar.png" alt='lama dev' fill/>
+          </div>
+          <div className="hidden xxl:flex flex-col">
+            <span className='font-bold'>Lama Dev</span>
+            <span className='text-sm text-textGray'>@lamawebDev</span>
+          </div>
+        </div>
+        <div className="hidden xxl:block cursor-pointer font-bold">...</div>
+      </div>
     </div>
   )
 }
